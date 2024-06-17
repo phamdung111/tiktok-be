cd9f2155708b466a3392da51c290185f6b811913
phamdung111 <88172121+phamdung111@users.noreply.github.com>
2024-06-17 16:52:19 +0700

    Change docker version (#5)
    * Change docker version
    
    * Update
    
    * Update

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -11,8 +11,20 @@ class SearchController extends Controller
 {
     public function search($content) {
         try{
-            $result = DB::table('posts')->where('posts.text','LIKE','%'.$content.'%')->get();
-            return response()->json(new AllPostCollection($result),200);
+            $searchAccount = DB::table('users')
+            ->where('name','like','%' . $content . '%')
+            ->join('follows','users.id','=','follows.user_id_creator')
+            ->select(
+                'users.id',
+                'users.name',
+                'users.bio',
+                DB::raw("CONCAT('" . url('/') . "', users.image) as image"),
+                DB::raw("COUNT(follows.user_id_creator) as follower")
+            )
+            ->groupBy('users.id')
+            ->get();
+            $searchPosts = DB::table('posts')->where('posts.text','like','%'.$content.'%')->get();
+            return response()->json(['users'=>$searchAccount, 'posts'=> new AllPostCollection($searchPosts)],200);
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 400);
         }
